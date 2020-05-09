6e14da584d07c76c29258048329826e020ad324a
ssuvorov <s.suvoroff@gmail.com>
2020-05-09 19:11:26 +0200

    init

--- next.config.js
+++ next.config.js
@@ -1,22 +1,26 @@
 const path = require('path');
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
+const withImages = require('next-images')
 
-module.exports = withCSS(withSass({
-	cssModules: true,
-	sassOptions: {
-		includePaths: [path.resolve(__dirname, "styles")]
-	},
-	webpack: (config, {defaultLoaders}) => {
-		config.module.rules.push({
-			test: /\.(js|jsx)$/,
-			include: [path.resolve(__dirname, 'components')],
-			use: [defaultLoaders.babel],
-		})
+module.exports = withImages({
+	...withCSS(withSass({
+		cssModules: true,
+		sassOptions: {
+			includePaths: [path.resolve(__dirname, "styles")]
+		},
+		webpack: (config, {defaultLoaders}) => {
+			config.module.rules.push({
+				test: /\.(js|jsx)$/,
+				include: [path.resolve(__dirname, 'components')],
+				use: [defaultLoaders.babel],
+			}),
 
-		// config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
-		config.resolve.alias['components'] = path.resolve(__dirname, 'components');
-		// config.resolve.alias['styles'] = path.resolve(__dirname, 'styles');
-		return config;
-	}
-}))
+			// config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
+			config.resolve.alias['components'] = path.resolve(__dirname, 'components');
+			config.resolve.alias['public'] = path.resolve(__dirname, 'public');
+			// config.resolve.alias['styles'] = path.resolve(__dirname, 'styles');
+			return config;
+		}
+	}))
+})
